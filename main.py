8cc359332eba862f4cf91c5191d7ea861c28f86a
roman <roman.pso.an@gmail.com>
2024-05-14 22:38:35 +0500

    Исправил ошибку в ui файле

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-import design
+import sys
+
 from PyQt5 import QtWidgets
+from design_ui import Ui_MainWindow
+import design_ui
 
+class Regi(QtWidgets.QMainWindow, design_ui.Ui_MainWindow):
+    def __init__(self):
+        super(Regi, self).__init__()
+        self.setupUi(self)
 
+app = QtWidgets.QApplication([])  # передаем пустой список
+window = Regi()
+window.show()
+
+app.exec()
